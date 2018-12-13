7526b9fb1bc86aa1ca7f2e1cfe803a66c767e4eb
11-diego-11 <fs2_macarrao@hotmail.com>
2018-12-13 20:58:13 -0200

    ajuste de estilo

--- main.js
+++ main.js
@@ -1,50 +1,49 @@
-
-
-function preencherPerfil (info) {
-	var template =	`
-	<h1>Aula 6 - AJAX</h1>
-	
-		<figure>
-			<img src="" alt="">
-		</figure>
-		<h1>${info.name}</h1>
-			<ul>
-				<li>
-					<span>Altura:</span>
-					<span>${info.height+"cm"}</span>
-				</li>
-				<li>
-					<span>cor do cabelo:</span>
-					<span>${info.hair_color}</span>
-				</li>
-				<li>
-					<span>Ano do nascimento:</span>	
-					<span>${info.birth_year}</span>	
-				</li>
-				<li>
-					<span>Naves:</span>
-					<span"><ul></ul></span>
-					
-				</li>
-			</ul>
-	
+function preencherPerfil(info) {
+	var template = `
+		<h2>${info.name}</h2>
+		<ul>
+			<li>
+				<span>Altura:</span>
+				<span>${info.height}</span>
+			</li>
+			<li>
+				<span>Cor do cabelo:</span>
+				<span>${info.hair_color}</span>
+			</li>
+			<li>
+				<span>Ano de nascimento:</span>
+				<span>${info.birth_year}</span>
+			</li>
+			<li>
+				<span>Naves:</span>
+				<span id="naves">
+					<ul>
+						${
+							info.starships.map(function(nave, i){
+								return `
+									<li>
+										<a href='${nave}'>
+											Nave ${i + 1}
+										</a>
+									</li>
+								`
+							}).join('')
+						}
+					</ul>
+				</span>
+			</li>
+		</ul>
 	`
 
-	// nome.innerHTML = info.name;
-	// altura.innerHTML = info.height + "cm";
-	// cabelo.innerHTML = info.hair_color;
-	// nascimento.innerHTML = info.birth_year;
-
 	var perfil = document.getElementById("perfil");
-	perfil.innerHTML = template; 
+	perfil.innerHTML = template;
 }
-	
-	
+
+
+// AJAX com jQuery
 $.ajax({
-	url: "https://swapi.co/api/people/11/",
-	method: "GET"
+	url: "https://swapi.co/api/people/1/"
 })
 .done(function(info){
-preencherPerfil(info)
+	preencherPerfil(info)
 })
-
